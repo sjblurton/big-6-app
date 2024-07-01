0229eb07ec4b21e3fd91ea382e56d4b97bc9a86c
sjlurton <sblurton@hotmail.co.uk>
2024-07-01 21:00:32 +0100

    feat: Add new colors for secondary light and dark shades
    This commit adds new color variables for the secondary light and dark shades in the `_variables.scss` file. It also adds corresponding TypeScript declarations in the `_exports.module.scss.d.ts` file. Additionally, the `Logo` component's SCSS file is updated to use the new colors for the `light` and `end` classes.

---
+++ src/styles/colors/_exports.module.scss.d.ts
@@ -0,0 +1,5 @@
+export declare const primary: string;
+export declare const primaryLight: string;
+export declare const secondaryDark: string;
+export declare const secondaryLight: string;
+export declare const white: string;
