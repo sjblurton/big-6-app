530c690754323588ccfabcc91b6761e68125afde
sjlurton <sblurton@hotmail.co.uk>
2024-07-13 13:47:54 +0100

    refactor: Update SCSS files to include black and various shades of grey

--- src/styles/colors/_exports.module.scss.d.ts
+++ src/styles/colors/_exports.module.scss.d.ts
@@ -1,3 +1,9 @@
+export declare const black: string;
+export declare const grey500: string;
+export declare const grey600: string;
+export declare const grey700: string;
+export declare const grey800: string;
+export declare const grey900: string;
 export declare const primary: string;
 export declare const primaryLight: string;
 export declare const secondaryDark: string;
