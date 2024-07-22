674f3f539d7625cd4b90035b9d8384825bfe4aea
sjlurton <sblurton@hotmail.co.uk>
2024-07-22 21:10:04 +0100

    refactor: storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,10 @@
-import type {Preview} from "@storybook/react";
-import {withThemeProvider} from "storybook-addon-theme-provider";
+import type { Preview } from "@storybook/react";
+import { withThemeProvider } from "storybook-addon-theme-provider";
 import MuiThemeProvider from "../src/modules/components/library/ThemeProvider/MuiThemeProvider";
 import "../src/styles/main.scss";
 import "../src/styles/base/reset.scss";
 import "../src/styles/base/global.scss";
-import {themes} from "@storybook/theming";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
