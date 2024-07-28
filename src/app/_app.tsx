df686e6a477c1d5cb0256cfc555f775e91be8e0a
sjlurton <sblurton@hotmail.co.uk>
2024-07-28 09:39:36 +0100

    chore: Update npm dependencies for react-query and eslint-plugin-query

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -3,9 +3,16 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
