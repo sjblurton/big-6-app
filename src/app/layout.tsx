be0e4bf90b68d0e1bd260a3d3a29c6605f6d70f5
sjlurton <sblurton@hotmail.co.uk>
2024-07-31 08:05:59 +0100

    chore: Add QueryProvider to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "../styles/main.scss";
 
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import QueryProvider from "@/modules/tanstackQuery/QueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
