39b5cf29e6bced2849d9bb2e85aee0466c13a771
Simon Blurton <sblurton@hotmail.co.uk>
2024-09-24 14:27:09 +0100

    refactor: Update import path for WorkoutController in workout.controller.ts

--- src/app/api/v1/workouts/[id]/controller/workout.controller.ts
+++ src/app/api/v1/workouts/[id]/controller/workout.controller.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { type WorkoutIds } from "@/modules/model/api/routes/workouts/inputs/inputs"
-import { WorkoutService } from "../services/workout.service"
+import { WorkoutService } from "../../../../../../modules/api/services/workout.service"
 
 class WorkoutController {
     private readonly workoutService: WorkoutService
@@ -11,6 +11,10 @@ class WorkoutController {
         this.workoutId = params.id
         this.workoutService = new WorkoutService(request, params.id)
     }
+
+    // This controller should validate the params i.e. this.workoutService.validateParams(params.id)
+
+    // It should request from the service what data it wants i.e. this.workoutService.getWorkoutData(validated.params.id)
 
     async GET() {
         return NextResponse.json(this.workoutService.getServiceData())
