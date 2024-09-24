6dc2f00d5ce6740bcdec6ab1c4c58b1bdca12ba9
Simon Blurton <sblurton@hotmail.co.uk>
2024-09-24 14:27:40 +0100

    feat: Add push-up exercise data and schema

---
+++ src/modules/model/exercises/data/push-up.ts
@@ -0,0 +1,271 @@
+import { type ExerciseStep } from "../schema"
+
+export const pushUps: ExerciseStep[] = [
+    {
+        step: 1,
+        name: "Wall Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 10,
+            },
+            intermediate: {
+                sets: 1,
+                reps: 25,
+            },
+            advanced: {
+                sets: 1,
+                reps: 50,
+            },
+        },
+        directions: {
+            positive:
+                "Stand facing a wall with your feet shoulder-width apart. Lean towards the wall until your nose almost touches it.",
+            negative:
+                "Push back to the starting position, making sure to keep your body in a straight line and your core engaged.",
+        },
+
+        video: "https://youtu.be/N5C9NUHZ20U",
+    },
+    {
+        step: 2,
+        name: "Incline Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 10,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 20,
+            },
+            advanced: {
+                sets: 3,
+                reps: 40,
+            },
+        },
+        directions: {
+            positive:
+                "Place your hands on an elevated surface, such as a bench or step, with your body in a straight line from head to heels.",
+            negative:
+                "Lower your chest towards the elevated surface, then push back up to the starting position.",
+        },
+
+        video: "https://youtu.be/Gv8y_prZBZY",
+    },
+    {
+        step: 3,
+        name: "Kneeling Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 10,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 15,
+            },
+            advanced: {
+                sets: 3,
+                reps: 30,
+            },
+        },
+        directions: {
+            positive:
+                "Start on your hands and knees, with your hands slightly wider than shoulder-width apart.",
+            negative:
+                "Lower your chest to the ground, then push back up to the starting position, keeping your core engaged and your body straight.",
+        },
+
+        video: "https://youtu.be/NyzxeqY6CR8",
+    },
+    {
+        step: 4,
+        name: "Half Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 8,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 12,
+            },
+            advanced: {
+                sets: 2,
+                reps: 25,
+            },
+        },
+        directions: {
+            positive:
+                "Assume a push-up position with your hands slightly wider than shoulder-width apart and your body in a straight line.",
+            negative:
+                "Lower your body halfway down, then push back up to the starting position.",
+        },
+
+        video: "https://youtu.be/bGuUODcwnHA",
+    },
+    {
+        step: 5,
+        name: "Full Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 5,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 10,
+            },
+            advanced: {
+                sets: 2,
+                reps: 20,
+            },
+        },
+
+        directions: {
+            positive:
+                "Start in a plank position with your hands under your shoulders and your body in a straight line.",
+            negative:
+                "Lower your body until your chest nearly touches the ground, then push back up to the starting position. Keep your elbows at a 45-degree angle to your body.",
+        },
+
+        video: "https://youtu.be/1QJICN6udbs",
+    },
+    {
+        step: 6,
+        name: "Close Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 5,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 10,
+            },
+            advanced: {
+                sets: 2,
+                reps: 20,
+            },
+        },
+
+        directions: {
+            positive:
+                "Begin in a push-up position with your hands closer together, directly under your chest.",
+            negative:
+                "Lower your body, keeping your elbows close to your sides, then push back up to the starting position.",
+        },
+
+        video: "https://youtu.be/3-1vRVuWgBc",
+    },
+    {
+        step: 7,
+        name: "Uneven Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 5,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 10,
+            },
+            advanced: {
+                sets: 2,
+                reps: 20,
+            },
+        },
+
+        directions: {
+            positive:
+                "Place one hand on an elevated surface and the other hand on the floor. Assume a push-up position with your body in a straight line.",
+            negative:
+                "Lower your body until your chest nearly touches the ground, then push back up to the starting position. Switch hands after completing the set.",
+        },
+        video: "https://youtu.be/o1abTRdwpUs",
+    },
+    {
+        step: 8,
+        name: "Half One Arm Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 5,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 10,
+            },
+            advanced: {
+                sets: 2,
+                reps: 20,
+            },
+        },
+
+        directions: {
+            positive:
+                "Assume a push-up position with one arm behind your back and your other arm in a regular push-up position.",
+            negative:
+                "Lower your body halfway down, then push back up to the starting position. Keep your core engaged and your body straight.",
+        },
+
+        video: "https://youtu.be/63077t3I4Zc",
+    },
+    {
+        step: 9,
+        name: "Levered Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 5,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 10,
+            },
+            advanced: {
+                sets: 2,
+                reps: 20,
+            },
+        },
+
+        directions: {
+            positive:
+                "Start in a push-up position with one hand placed further forward than the other. Keep your body in a straight line.",
+            negative:
+                "Lower your body until your chest nearly touches the ground, then push back up to the starting position. Switch hand positions after completing the set.",
+        },
+
+        video: "https://youtu.be/Hwq5zdb-owA",
+    },
+    {
+        step: 10,
+        name: "On Arm Push Up",
+        progressions: {
+            beginner: {
+                sets: 1,
+                reps: 5,
+            },
+            intermediate: {
+                sets: 2,
+                reps: 10,
+            },
+            advanced: {
+                sets: 2,
+                reps: 100,
+            },
+        },
+        directions: {
+            positive:
+                "Begin in a push-up position with one arm behind your back and the other arm in a regular push-up position.",
+            negative:
+                "Lower your body until your chest nearly touches the ground, then push back up to the starting position.",
+        },
+
+        video: "https://youtu.be/ReKZry7JQEQ",
+    },
+] as const
+
+export const pushUpDescription =
+    "The push up is the ultimate upper body exercise. It generates strength, builds muscle, develops powerful tendons and trains the upper body pressing the muscles to work in coordination with the midsection, and lower body"
---
+++ src/modules/model/exercises/exercises.ts
@@ -0,0 +1,12 @@
+import { pushUpDescription } from "./data/push-up";
+import { type Exercise } from "./schema";
+
+export const exercises: Exercise[] = [
+    {
+        key: "f0abbf89-3c9b-49e1-ad47-d72ec67ae454",
+        title: "Push Up",
+        description: pushUpDescription,
+        steps:
+    },
+]
+
