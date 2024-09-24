8d39014af1a5eb5f10c8dda06f7806aefa60f6c1
Simon Blurton <sblurton@hotmail.co.uk>
2024-09-24 11:34:51 +0100

    feat: Add Storybook manager and theme configuration

---
+++ .storybook/theme.ts
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming"
+
+export default create({
+    base: "dark",
+    brandTitle: "Big 6 Documentation",
+    brandImage: "https://i.ibb.co/hYvW0y8/storybook-logo.jpg",
+    brandTarget: "_self",
+    fontBase: '"Inter", sans-serif',
+    fontCode: "monospace",
+})
