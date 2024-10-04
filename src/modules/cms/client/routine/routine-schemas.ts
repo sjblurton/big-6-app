c91d00f4496df0fa93d3c90b4337e9196d1c7ac2
Simon Blurton <sblurton@hotmail.co.uk>
2024-10-04 17:01:42 +0100

    refactor: Update routine-client.ts and routine-schemas.ts
    - Update routine-client.ts to include new functions for creating rest day data, sorting days, mapping routine days, validating routine days, and transforming routines.
    - Update routine-schemas.ts to include new exercise names and week day values.

--- src/modules/cms/client/routine/routine-schemas.ts
+++ src/modules/cms/client/routine/routine-schemas.ts
@@ -1,15 +1,34 @@
 import { z } from "zod"
 
+import { exerciseNames } from "@/modules/contants/exercise-names"
+import { weekDays } from "@/modules/contants/week-days"
+
 import { documentBaseSchema, imageSchema } from "../shared/base-schemas"
 
 const exerciseSchema = z.object({
-    _key: z.string(),
+    _id: z.string(),
     image: imageSchema,
-    name: z.string(),
+    name: z.enum([
+        exerciseNames.bridge.value,
+        exerciseNames.handstand.value,
+        exerciseNames.legRaise.value,
+        exerciseNames.pullUp.value,
+        exerciseNames.pushUp.value,
+        exerciseNames.squat.value,
+        "rest",
+    ]),
 })
 
 const daySchema = z.object({
-    name: z.string(),
+    name: z.enum([
+        weekDays.monday.value,
+        weekDays.tuesday.value,
+        weekDays.wednesday.value,
+        weekDays.thursday.value,
+        weekDays.friday.value,
+        weekDays.saturday.value,
+        weekDays.sunday.value,
+    ]),
     _key: z.string(),
     exercises: exerciseSchema.array(),
 })
@@ -20,3 +39,7 @@ export const routineDocumentSchema = documentBaseSchema.extend({
     name: z.string(),
     days: daySchema.array(),
 })
+
+export type RoutineDocument = z.infer<typeof routineDocumentSchema>
+
+export type RoutineDay = z.infer<typeof daySchema>
