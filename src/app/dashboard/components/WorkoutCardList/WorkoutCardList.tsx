fc848a8459a1ce46c416f62356c3d754a40786fb
Simon Blurton <sblurton@hotmail.co.uk>
2024-10-05 11:52:58 +0100

    refactor: Update WorkoutCardList component

--- src/app/dashboard/components/WorkoutCardList/WorkoutCardList.tsx
+++ src/app/dashboard/components/WorkoutCardList/WorkoutCardList.tsx
@@ -1,77 +1,58 @@
 "use client"
 
 import { useQuery } from "@tanstack/react-query"
-import React, { useMemo } from "react"
+import React from "react"
 
 import { workoutAxiosClient } from "@/modules/api/client/workouts"
-import { createUUIDs } from "@/modules/arrays/uuids"
-import { MuiGrid } from "@/modules/components/library/mui"
+import { MuiGrid, MuiTypography } from "@/modules/components/library/mui"
 import { WORKOUTS_QUERY_KEY } from "@/modules/tanstackQuery/keys/query-keys"
 
+import WorkoutCardListSkeleton from "./WorkoutCardListSkeleton"
+
 import WorkoutCard from "../WorkoutCard/WorkoutCard"
-import WorkoutCardSkeleton from "../WorkoutCard/WorkoutCardSkeleton"
 
 function WorkoutCardList() {
-    const uuids = useMemo(() => createUUIDs(6), [])
     const { data, isLoading, isError, error } = useQuery({
         queryKey: [WORKOUTS_QUERY_KEY],
         queryFn: () => workoutAxiosClient.getWorkouts(),
     })
 
     if (isLoading) {
-        return (
-            <MuiGrid container mt={3} mb={1}>
-                {uuids.map((value) => (
-                    <MuiGrid
-                        item
-                        xs={12}
-                        key={value}
-                        display="flex"
-                        height="100%"
-                        alignContent="center"
-                        justifyContent="center"
-                    >
-                        <WorkoutCardSkeleton />
-                    </MuiGrid>
-                ))}
-            </MuiGrid>
-        )
+        return <WorkoutCardListSkeleton />
     }
 
     if (isError || !data) {
         throw error
     }
 
-    if (data.length === 0) {
-        return (
-            <MuiGrid container mt={3} mb={1}>
-                <MuiGrid
-                    item
-                    xs={12}
-                    display="flex"
-                    alignContent="center"
-                    justifyContent="center"
-                >
-                    <h2>You have not yet logged and workouts</h2>
-                </MuiGrid>
-            </MuiGrid>
-        )
-    }
-
     return (
         <MuiGrid container mt={3} mb={1}>
-            {data.map((workout) => (
+            {data.length > 0 ? (
+                data.map((workout) => (
+                    <MuiGrid
+                        item
+                        xs={12}
+                        key={workout.key}
+                        display="flex"
+                        alignContent="center"
+                        justifyContent="center"
+                    >
+                        <WorkoutCard workout={workout} />
+                    </MuiGrid>
+                ))
+            ) : (
                 <MuiGrid
                     item
                     xs={12}
-                    key={workout.key}
                     display="flex"
                     alignContent="center"
                     justifyContent="center"
                 >
-                    <WorkoutCard workout={workout} />
+                    <MuiTypography variant="h4" component="h2">
+                        You have not yet logged any workouts
+                    </MuiTypography>
                 </MuiGrid>
-            ))}
+            )}
         </MuiGrid>
     )
 }
