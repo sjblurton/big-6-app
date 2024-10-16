facbc1ee0d8d1a99a4cc219864285efe5da264e4
Simon Blurton <sblurton@hotmail.co.uk>
2024-10-16 17:23:14 +0100

    Add LevelsRadio test

---
+++ src/app/add/Form/steps/LevelsRadio/LevelsRadio.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react"
+import React from "react"
+
+import LevelsRadio from "./LevelsRadio"
+
+import { useCreateFormContextInputs } from "../../hooks/use-create-form-context"
+
+jest.mock("../../hooks/use-create-form-context", () => ({
+    useCreateFormContextInputs: jest.fn(),
+}))
+
+jest.mock("./LevelRadioButton/LevelRadioButton", () => {
+    const MockRadioButton = (properties: { [key: string]: unknown }) => (
+        <div data-testid="level-radio-button" {...properties} />
+    )
+    return MockRadioButton
+})
+
+jest.mock("../StepError/StepError", () => {
+    const MockStepError = (properties: { [key: string]: unknown }) => (
+        <div data-testid="step-error" {...properties} />
+    )
+    return MockStepError
+})
+
+describe("LevelsRadio", () => {
+    it("should render the correct number of LevelRadioButton components", () => {
+        ;(useCreateFormContextInputs as jest.Mock).mockReturnValue({
+            formState: { errors: {} },
+        })
+
+        render(<LevelsRadio />)
+
+        const levelRadioButtons = screen.getAllByTestId("level-radio-button")
+        expect(levelRadioButtons).toHaveLength(10)
+    })
+
+    it("should display an error message when there is a form error", async () => {
+        ;(useCreateFormContextInputs as jest.Mock).mockReturnValue({
+            formState: { errors: { workout: { level: "Error" } } },
+        })
+
+        render(<LevelsRadio />)
+
+        const stepError = screen.getByTestId("step-error")
+        expect(stepError).toHaveAttribute("message", "Please select a level")
+    })
+})
