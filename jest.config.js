d99688483a3c1d437bf253af3080ef5ec1690ed7
Simon Blurton <sblurton@hotmail.co.uk>
2024-10-16 11:38:16 +0100

    Refactor Jest config to use TypeScript and treat files as ESM
    feat: test for the breadcrumbs

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports -- jest config
+// eslint-disable-next-line @typescript-eslint/no-require-imports, unicorn/prefer-module -- jest config
 const nextJest = require("next/jest")
 
 const createJestConfig = nextJest({
@@ -6,6 +6,13 @@ const createJestConfig = nextJest({
 })
 const customJestConfig = {
     moduleDirectories: ["node_modules", "<rootDir>/"],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    extensionsToTreatAsEsm: [".ts", ".tsx"],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest",
+    },
     testEnvironment: "jest-environment-jsdom",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
     collectCoverage: true,
@@ -21,6 +28,11 @@ const customJestConfig = {
             lines: 90,
             statements: 90,
         },
+        "ts-jest": {
+            useESM: true,
+        },
     },
 }
+
+// eslint-disable-next-line unicorn/prefer-module -- jest config
 module.exports = createJestConfig(customJestConfig)
---
+++ src/app/add/Form/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,74 @@
+import { render, screen } from "@testing-library/react"
+import React from "react"
+import { useWatch } from "react-hook-form"
+
+import FormBreadcrumbs from "./Breadcrumbs"
+
+import { useCreateFormContextInputs } from "../hooks/use-create-form-context"
+
+jest.mock("react-hook-form", () => ({
+    useWatch: jest.fn(),
+}))
+
+jest.mock("../hooks/use-create-form-context", () => ({
+    useCreateFormContextInputs: jest.fn(),
+}))
+
+jest.mock("@/modules/components/library/mui", () => ({
+    MuiBox: ({ children }: { children: React.ReactNode }) => (
+        <div>{children}</div>
+    ),
+    MuiStep: ({ children }: { children: React.ReactNode }) => (
+        <div>{children}</div>
+    ),
+    MuiStepLabel: ({ children }: { children: React.ReactNode }) => (
+        <div>{children}</div>
+    ),
+    MuiStepper: ({
+        children,
+        activeStep,
+    }: {
+        children: React.ReactNode
+        activeStep: number
+    }) => (
+        <div data-testid="stepper" data-active-step={activeStep}>
+            {children}
+        </div>
+    ),
+}))
+
+describe("FormBreadcrumbs", () => {
+    const mockControl = {}
+    const mockUseCreateFormContextInputs = {
+        control: mockControl,
+    }
+
+    beforeEach(() => {
+        ;(useCreateFormContextInputs as jest.Mock).mockReturnValue(
+            mockUseCreateFormContextInputs
+        )
+    })
+
+    it("renders the steps correctly", () => {
+        ;(useWatch as jest.Mock).mockReturnValue(0)
+
+        render(<FormBreadcrumbs />)
+
+        expect(screen.getByText("Select Exercise")).toBeInTheDocument()
+        expect(screen.getByText("Select Level")).toBeInTheDocument()
+        expect(screen.getByText("Log Workout")).toBeInTheDocument()
+    })
+
+    it("sets the active step correctly", () => {
+        const activeStep = 1
+        ;(useWatch as jest.Mock).mockReturnValue(activeStep)
+
+        render(<FormBreadcrumbs />)
+
+        const stepper = screen.getByTestId("stepper")
+        expect(stepper).toHaveAttribute(
+            "data-active-step",
+            activeStep.toString()
+        )
+    })
+})
