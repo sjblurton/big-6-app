f054b4ecbcadb2326be8ea92da9f6da34ff1a795
Simon Blurton <sblurton@hotmail.co.uk>
2024-10-16 17:10:13 +0100

    Add LevelRadioButton test

---
+++ src/app/add/Form/steps/LevelsRadio/LevelRadioButton/LevelRadioButton.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react"
+import React from "react"
+
+import LevelRadioButton from "./LevelRadioButton"
+
+jest.mock("@/modules/components/ui/form-inputs/RadioButton/RadioButton", () => {
+    const MockRadioButton = (properties: { [key: string]: unknown }) => (
+        <div data-testid="radio-button" {...properties} />
+    )
+    MockRadioButton.displayName = "MockRadioButton"
+    return MockRadioButton
+})
+
+describe("LevelRadioButton", () => {
+    it("should render the LevelRadioButton component with the correct label and value", () => {
+        const level = "1"
+
+        render(<LevelRadioButton level={level} />)
+
+        const radioButton = screen.getByTestId("radio-button")
+        expect(radioButton).toBeInTheDocument()
+        expect(radioButton).toHaveAttribute("value", level)
+        expect(radioButton).toHaveAttribute("name", "workout.level")
+    })
+})
